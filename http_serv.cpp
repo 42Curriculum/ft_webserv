0ce028c9480a6c829b989437b2de34cae3881a76
SEUNGHYUN <kosehy@gmail.com>
2020-06-01 08:56:20 -0700

    Update files
    * Update http_serv.cpp
    - Add ioctl for non-blocking I/O
    - Add time_out for checking timeout
    
    * Update client.cpp
    - Change port from 8080 to 80
    
    * To-do
    - Add read and write

--- http_serv.cpp
+++ http_serv.cpp
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "server.hpp"
+#include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <errno.h>
@@ -19,153 +20,258 @@
 #define PORT 80
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket;
-	long valread;
-    struct sockaddr_in address;
+    int desc_ready, close_conn, end_server = 0;
     int client[30];
-    int i, check_socket, max_sd, sd;
+    int i, check_socket, rc, len, on, listen_sd, max_sd, new_sd;
     int max_client = 30;
-    int addrlen = sizeof(address);
     char buffer[1025];
+    struct sockaddr_in address;
+    struct timeval time_out;
 	std::list<Data> servers;
     std::string ss;
 
 	// servers = init();
     // set of socket descriptors
-    fd_set read_fd;
+    fd_set master_set, working_set;
     
     // Only this line has been changed. Everything is same.
     char hello[] = {"HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!"};
     
-    // initialise all client arrays ot 0
+    /**************************************************************************/
+    /* initialise all client arrays to 0                                      */
+    /**************************************************************************/
     for (i = 0; i < max_client; ++i)
     {
         client[i] = 0;
     }
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    /**************************************************************************/
+    /* Create an AF_INET stream socket to receive incoming connections on     */
+    /**************************************************************************/
+    listen_sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sd == 0)
     {
-        perror("In socket");
+        perror("socket() failed");
         exit(EXIT_FAILURE);
-    }    
+    }
 
+    /**************************************************************************/
+    /* Allow socket descriptor to be reuseable                                */
+    /**************************************************************************/
+    rc = setsockopt(listen_sd, SOL_SOCKET, SO_REUSEADDR,
+                    (char *)&on, sizeof(on));
+    if (rc < 0)
+    {
+        perror("set_socketopt() failed");
+        close(listen_sd);
+        exit(-1);
+    }
+
+    /**************************************************************************/
+    /* Set socket to be nonblocking. ALl of the sockets for the incoming      */
+    /* connections will also be nonblocking since they will inherit that      */
+    /* state from the listening socket.                                       */
+    /**************************************************************************/
+    rc = ioctl(listen_sd, FIONBIO, (char *)&on);
+    if (rc < 0)
+    {
+        perror("ioctl() failed");
+        close(listen_sd);
+        exit(-1);
+    }
+
+    /**************************************************************************/
+    /* Bind the socket                                                        */
+    /**************************************************************************/
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
-    
-    memset(address.sin_zero, '\0', sizeof address.sin_zero);
-        
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    rc = bind(listen_sd, (struct sockaddr *)&address, sizeof(address));
+    if (rc < 0)
     {
-        perror("In bind");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 10) < 0)
-    {
-        perror("In listen");
+        perror("bind() failed");
+        close(listen_sd);
         exit(EXIT_FAILURE);
     }
 
-    // accept the incoming connection
-    std::cout << "Waiting for connections ..." << std::endl;
-
-    while(1)
+    /**************************************************************************/
+    /* Set the listen back log                                                */
+    /**************************************************************************/
+    rc = listen(listen_sd, 32);
+    if (rc < 0)
     {
-        // clear the socket set
-        FD_ZERO(&read_fd);
-
-        // add socket to set
-        FD_SET(server_fd, &read_fd);
-        max_sd = server_fd;
-
-        // add child sockets to set
-        for (i = 0; i < max_client; ++i)
-        {
-            // socket descriptor
-            sd = client[i];
-
-            // if socket descriptor is valid, then add it to read list
-            if (sd > 0)
-                FD_SET(sd, &read_fd);
-
-            // If the descriptor number is over the max, set the max_sd as the number of descriptor
-            if (sd > max_sd)
-                max_sd = sd;
-        }
-
-        // wait for an check_socket on one of the sockets, timeout is NULL,
-        // so wait indefinitely
-        check_socket = select(max_sd + 1, &read_fd, NULL, NULL, NULL);
-
-        if ((check_socket < 0) && (errno != EINTR))
-        {
-            std::cout<< "select error" << std::endl;
-        }
-        //If something happened on the new_socket, then it's an incoming connection
-        if (FD_ISSET(server_fd, &read_fd))
-        {
-            if ((new_socket = accept(server_fd,
-                    (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-            {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
-
-            //inform the user of socket number which used in send and receive commands
-            std::cout << "New connection , socket fd is " << std::to_string(new_socket);
-            std::cout << " , ip is : " << inet_ntoa(address.sin_addr);
-            std::cout << " , port : " << std::to_string(ntohs(address.sin_port)) << std::endl;
-            //send new connection message
-            if( send(new_socket, hello, strlen(hello), 0) != strlen(hello) )
-            {
-                perror("send");
-            }
-
-            std::cout << "Welcome message sent successfully" << std::endl;
-
-            //add the new socket into the array of sockets
-            for (i = 0; i < max_client; i++)
-            {
-                //if position is empty
-                if( client[i] == 0 )
-                {
-                    client[i] = new_socket;
-                    std::cout << "Adding to list of sockets as " << std::to_string(i) << std::endl;
-                    break;
-                }
-            }
-        }
-
-        //else it's some IO operation on some other socket 
-        for (i = 0; i < max_client; i++)
-        {
-            sd = client[i];
-
-            if (FD_ISSET( sd , &read_fd))
-            {
-                //Check if it was for closing , and also read the incoming message
-                if ((valread = read( sd , buffer, 1024)) == 0)
-                {
-                    //if socket is disconnected, get the details and print them
-                    getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen);
-                    std::cout << "Host disconnected , ip " << inet_ntoa(address.sin_addr);
-                    std::cout << " , port " << std::to_string(ntohs(address.sin_port)) << std::endl;
-
-                    //Close the socket and mark as 0 in list for reuse
-                    close( sd );
-                    client[i] = 0;
-                }
-
-                //Echo back the message that came in
-                else 
-                {
-                    //set the string terminating NULL byte on the end of the data read
-                    buffer[valread] = '\0';
-                    send(sd , buffer , strlen(buffer) , 0 );
-                }
-            }
-        }
+        perror("listen() failed");
+        close(listen_sd);
+        exit(EXIT_FAILURE);
     }
-    return 0;
+
+    /**************************************************************************/
+    /* Initialize the master fd_set                                           */
+    /**************************************************************************/
+    FD_ZERO(&master_set);
+    max_sd = listen_sd;
+    FD_SET(listen_sd, &master_set);
+
+    /**************************************************************************/
+    /* Initialize the time_out to 3 minutes. If no activity after 3 minutes,  */
+    /* this program will end                                                  */
+    /**************************************************************************/
+    time_out.tv_sec = 3 * 60;
+    time_out.tv_usec = 0;
+
+    /**************************************************************************/
+    /* Loop waiting for incoming connects or for incoming data on any of the  */
+    /* connected sockets.                                                     */
+    /**************************************************************************/
+    do
+    {
+        /**********************************************************************/
+        /* Copy the master fd_set over to the working fd_set.                 */
+        /**********************************************************************/
+        memcpy(&working_set, &master_set, sizeof(master_set));
+
+        /**********************************************************************/
+        /* Call select() and wait 3 minutes for it to complete.               */
+        /**********************************************************************/
+        std::cout << "Waiting on select()..." << std::endl;
+        rc = select(max_sd + 1, &working_set, NULL, NULL, &time_out);
+
+        /**********************************************************************/
+        /* Check to see if the select call failed.                            */
+        /**********************************************************************/
+        if (rc < 0)
+        {
+            perror(" select() failed");
+            break;
+        }
+
+        /**********************************************************************/
+        /* Check to see if the 3 minute time out expired.                     */
+        /**********************************************************************/
+        if (rc == 0)
+        {
+            std::cout << " select() timed out. End program." << std::endl;
+            break;
+        }
+
+        /**********************************************************************/
+        /* One or more descriptors are are readable. Need to determine        */
+        /* which ones they are.                                               */
+        /**********************************************************************/
+        desc_ready = rc;
+        for (i = 0; i <= max_sd && desc_ready > 0; ++i)
+        {
+            // Check to see if this descriptor is ready
+            if (FD_ISSET(i, &working_set))
+            {
+                // A Descriptor was found that was readable - one less has to be
+                // lokked for. This is being done so that we can stop looking at
+                // the working set once we have found all of the descriptors
+                // that were ready.
+                desc_ready -= 1;
+
+                // Check to see if this is the listening socket
+                if (i == listen_sd)
+                {
+                    std::cout << " Listening socket is readable" << std::endl;
+                    // Accept all incoming connections that are qeueued up on the
+                    // listenning socket before we loop back and call 
+                    // select again.
+                    do
+                    {
+                        // Accept each incoming conneciton. If accept fails with
+                        // EWOULDBLOCK, then we have accepted all of them. Any
+                        // other failure on accept will cause us to end the
+                        // server.
+                        new_sd = accept(listen_sd, NULL, NULL);
+                        if (new_sd < 0)
+                        {
+                            if (errno != EWOULDBLOCK)
+                            {
+                                perror(" accept() failed");
+                                end_server = 1;
+                            }
+                            break;
+                        }
+
+                        // Add the new incoming connections to the
+                        // master read set
+                        std::cout << " New incoming connection - ";
+                        std::cout << std::to_string(new_sd) << std::endl;
+                        FD_SET(new_sd, &master_set);
+                        if (new_sd > max_sd)
+                            max_sd = new_sd;
+                        
+                        // Loop back up and accept another incoming connection
+                    } while (new_sd != -1);
+                }
+
+                // This is not the listening socket, therefore an existing
+                // connection must be readable
+                else
+                {
+                    std::cout << " Descriptor " << std::to_string(i);
+                    std::cout << " is readable" << std::endl;
+                    close_conn = 0;
+                    // Receive all incoming data on this socket before we loop
+                    // back and call select again.
+                    do
+                    {
+                        // Receive data on this connection until the recv
+                        // fails with EWOULDBLOCK. If any other failure
+                        // occurs, we will close the connection.
+                        rc = recv(i, buffer, sizeof(buffer), 0);
+                        if (rc < 0)
+                        {
+                            if (errno != EWOULDBLOCK)
+                            {
+                                perror(" recv() failed");
+                                close_conn = 1;
+                            }
+                            break;
+                        }
+
+                        // CHeck to see if the connection has been closed
+                        // by the client
+                        if (rc == 0)
+                        {
+                            std::cout << " Connection closed" << std::endl;
+                            close_conn = 1;
+                            break;
+                        }
+
+                        // Data was received
+                        len = rc;
+                        std::cout << " " << std::to_string(len);
+                        std::cout << " bytes received" << std::endl;
+
+                        // Echo the data back to the client
+                        rc = send(i, buffer, len, 0);
+                        if (rc < 0)
+                        {
+                            perror(" send() failed");
+                            close_conn = 1;
+                            break;
+                        }
+                    } while (1);
+
+                    // If the close_conn flag was turned on, we need to clean up
+                    // this active connection. This clean up process includes
+                    // removing the descriptor from the master set and
+                    // determining the new maximum descriptor value based on
+                    // the bits that are still turned on in the master set.
+                    if (close_conn)
+                    {
+                        close(i);
+                        FD_CLR(i, &master_set);
+                        if (i == max_sd)
+                        {
+                            while (FD_ISSET(max_sd, &master_set) == 0)
+                                max_sd -= 1;
+                        }
+                    }
+                } // End of existing connection is readable
+            } // End of if (FD_ISSET(i, &working_set))
+        } // End of loop through selectable descriptors
+    } while (end_server == 0);
 }
